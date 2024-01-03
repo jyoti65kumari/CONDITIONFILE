71bca46e383cf4e1d9ee13b761bbd8d26a3d5d61
jyoti65kumari <jyotikumari22@navgurukul.org>
2024-01-04 00:19:38 +0530

    CONDITION

---
+++ Script.js
@@ -0,0 +1,12 @@
+let num = prompt("Enter your website name")
+if (num == "youtube"){
+    location.href="https://youtu.be/78VMefnkMcM?si=KsFEsqZk83X66LH0"
+} else if(num == "codechef"){
+    location.href="https://www.codechef.com/users/jyoti_56"
+} else if(num=="DOM"){
+    location.href="https://www.w3schools.com/js/js_htmldom_methods.asp"
+}else if(num =="30daysjs"){
+    location.href="https://30dayjavascript.js.org/"
+}else{
+    alert("you entered invalid website")
+}
